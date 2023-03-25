68c626183474162f3341b6353c68336d2d556855
Yury Mankevich <byvnis@gmail.com>
2023-03-25 13:58:15 +0300

    Сompletely rewritten

--- pl.sh
+++ pl.sh
@@ -1,17 +1,63 @@
 #!/bin/bash
 
-echo -n "|  |"
+######### orange waw # retn waw # orange gdn ####
+SERVERS="    4166       32927        7200       "
+LOGFILE=spdtst.pl.log
+LOGGING=0 #0 or 1
+#################################################
+
+spdtst () {
+ results=$(speedtest -s $1)
+ echo "$results" | tr -d '\r' | while read -r line
+ do
+  echo "$line" | grep Server
+  downl=$(echo "$line" | grep Download)
+  if [ ! -z "$downl" ]
+  then
+   set -- $downl
+   echo "$1 $2 $3"
+  fi
+  upl=$(echo "$line" | grep Upload)
+  if [ ! -z "$upl" ]
+  then
+   set -- $upl
+   echo "$1   $2 $3"
+  fi
+  ploss=$(echo "$line" | grep Packet)
+  if [ ! -z "$ploss" ]
+  then
+   set -- $ploss
+   echo "$1 $2 $3"
+  fi
+done
+}
+
+log () {
+ if [ -f "$LOGFILE" ]
+ then
+  echo "$results" >> $LOGFILE
+ else
+  echo "$results" > $LOGFILE
+ fi
+}
+
+if [ -f "$LOGFILE" ]
+then
+ rm $LOGFILE
+fi
+
+for server in $SERVERS
+do
+ if [ $LOGGING -eq 1 ]
+ then
+  spdtst $server && log
+ else
+  spdtst $server
+ fi
+done
 
 #warsaw orange
-results=$(speedtest -s 4166)
-#echo -e "$results" | while read -r line; do echo "$line"; done
-echo "$results" > spdtst.pl.log
-
-echo -ne "\r|. |"
+#spdtst "4166" && log
 
 #warsaw retn
-results=$(speedtest -s 32927)
-#echo -e "$results" | while read -r line; do echo "$line"; done
-echo "$results" >> spdtst.pl.log
-
-echo -e "\r|..|"
+#spdtst "32927" #&& log
