7084db47c8dccb44636fe45edca741fce64916af
Yury Mankevich <byvnis@gmail.com>
2023-03-31 13:51:19 +0300

    Line indents

--- pl.sh
+++ pl.sh
@@ -7,53 +7,53 @@ LOGGING=0 #0 or 1
 #################################################
 
 spdtst () {
- results=$(speedtest -s $1)
- echo "$results" | tr -d '\r' | while read -r line
- do
-  echo "$line" | grep Server
-  downl=$(echo "$line" | grep Download)
-  if [ ! -z "$downl" ]
-  then
-   set -- $downl
-   echo "$1 $2 $3"
-  fi
-  upl=$(echo "$line" | grep Upload)
-  if [ ! -z "$upl" ]
-  then
-   set -- $upl
-   echo "$1   $2 $3"
-  fi
-  ploss=$(echo "$line" | grep Packet)
-  if [ ! -z "$ploss" ]
-  then
-   set -- $ploss
-   echo "$1 $2 $3"
-  fi
-done
+  results=$(speedtest -s $1)
+  echo "$results" | tr -d '\r' | while read -r line
+  do
+    echo "$line" | grep Server
+    downl=$(echo "$line" | grep Download)
+    if [ ! -z "$downl" ]
+    then
+      set -- $downl
+      echo "$1 $2 $3"
+    fi
+    upl=$(echo "$line" | grep Upload)
+    if [ ! -z "$upl" ]
+    then
+      set -- $upl
+      echo "$1   $2 $3"
+    fi
+    ploss=$(echo "$line" | grep Packet)
+    if [ ! -z "$ploss" ]
+    then
+      set -- $ploss
+      echo "$1 $2 $3"
+    fi
+  done
 }
 
 log () {
- if [ -f "$LOGFILE" ]
- then
-  echo "$results" >> $LOGFILE
- else
-  echo "$results" > $LOGFILE
- fi
+  if [ -f "$LOGFILE" ]
+  then
+    echo "$results" >> $LOGFILE
+  else
+    echo "$results" > $LOGFILE
+  fi
 }
 
 if [ -f "$LOGFILE" ]
 then
- rm $LOGFILE
+  rm $LOGFILE
 fi
 
 for server in $SERVERS
 do
- if [ $LOGGING -eq 1 ]
- then
-  spdtst $server && log
- else
-  spdtst $server
- fi
+  if [ $LOGGING -eq 1 ]
+  then
+    spdtst $server && log
+  else
+    spdtst $server
+  fi
 done
 
 #warsaw orange
